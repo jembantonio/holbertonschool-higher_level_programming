93de09452da8b337956b77ff4d2fe1f34448ab57
Your Name <you@example.com>
2019-04-29 19:05:55 +0000

    finishing problem 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,1 +1,19 @@
-int has_cycle
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	slow = list;
+	fast = list->next;
+
+	while (slow != NULL && fast->next != NULL && fast != NULL)
+	{
+		if (slow == fast)
+			return (1);
+
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (0);
+}
