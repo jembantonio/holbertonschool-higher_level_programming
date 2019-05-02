f7879c445154c1557ca855abe03fe149730e37e1
jembantonio <jembantonio@gmail.com>
2019-05-02 20:37:31 +0000

    initial commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == '__main__':
+    ac = len(sys.argv) - 1
+
+    if ac == 0:
+        print("0 arguments.")
+    elif ac == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(ac))
+        for i in range(1, ac):
+            print("{}: {}".format(i, sys.argv[i]))
