6608a07c5bba7e0ff325d4bc6991063e90eb5148
jembantonio <jembantonio@gmail.com>
2019-05-07 21:46:23 +0000

    readme commit and problem 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+def divisible_by_2(my_list=[]):
+    if my_list is None:
+        return
+
+    div_two = []
+    for num in my_list:
+        if (num % 2 == 0):
+            div_two.append(True)
+        else:
+            div_two.append(False)
+
+    return div_two
