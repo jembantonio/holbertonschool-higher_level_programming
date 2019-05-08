55994cfbeb0ec6f46b2a5b322301a0bf75bea539
jembantonio <jembantonio@gmail.com>
2019-05-08 05:17:58 +0000

    added shebang

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,8 @@
-
+#!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list is None:
         return
     max = my_list[0]
-
     for num in my_list:
         if (max < num):
             max = num
