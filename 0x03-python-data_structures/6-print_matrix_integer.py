0cebf02107eb829340246380cb87e55330b7a9b4
jembantonio <jembantonio@gmail.com>
2019-05-08 05:15:22 +0000

    syntax error fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix is None:
-        return
+        print()
     for r in matrix:
         for i in r:
-            print("{}".format(i), end="")
+            print("{:d}".format(i), end="")
             if i != r[-1]:
                 print(" ", end="")
         print()
