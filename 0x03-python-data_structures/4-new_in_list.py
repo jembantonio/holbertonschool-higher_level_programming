8da3a6d7d1a1ecb1ead1b607f1ec8820bf29eeb7
jembantonio <jembantonio@gmail.com>
2019-05-08 05:09:02 +0000

    syntax error fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
-    if idx < 0 or idx > len(new_list) or new_list is None or idx is None:
-    or element is None:
+    if idx < 0 or idx > len(new_list) or new_list is None:
+        return new_list
+    if idx is None or element is None:
         return new_list
     new_list[idx] = element
     return new_list
