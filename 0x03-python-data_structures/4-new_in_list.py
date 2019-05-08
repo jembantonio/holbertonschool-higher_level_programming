fa3e2f54b271afd067cf7252ca9d5aaff9d5c12d
jembantonio <jembantonio@gmail.com>
2019-05-08 05:10:12 +0000

    added new edge case

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
+    if my_list = None:
+        return 
     if idx < 0 or idx > len(new_list) or new_list is None:
         return new_list
     if idx is None or element is None:
