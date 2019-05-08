868c6d067cdab032c3baf5d9d2bbc80b28f4f1ec
jembantonio <jembantonio@gmail.com>
2019-05-08 05:01:09 +0000

    added edge cases

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    if my_list is None:
+        return
+    if idx is None:
+        return
     if idx < 0:
-        return None
-    elif idx > len(my_list):
-        return None
+        return
+    if idx >= len(my_list):
+        return
     else:
         return my_list[idx]
