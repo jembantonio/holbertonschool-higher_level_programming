f8359b46951e853aa3783b67a0dc75061ad9cfb1
jembantonio <jembantonio@gmail.com>
2019-05-08 05:02:11 +0000

    added edge cases

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
+    if my_list is None:
+        return
+    if idx is None:
+        return
     if idx < 0:
         return my_list
     elif idx > len(my_list):
