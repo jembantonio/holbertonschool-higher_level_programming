11c51f39bc6be5556aa8c06523e0851bb5244dad
jembantonio <jembantonio@gmail.com>
2019-05-29 21:26:14 +0000

    pep8 fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-''' 
+'''
 add_integer
 
 This module adds two ints and/or floats
 '''
 
+
 def add_integer(a, b=98):
     '''add_integer function adds (a) and (b)
     Return: sum of (a) and (b)
     '''
-
     if not type(a) is int:
         if not type(a) is float:
             raise TypeError("a must be an integer")
