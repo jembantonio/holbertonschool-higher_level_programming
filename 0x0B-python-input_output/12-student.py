2298f724ae9060270f2d724685b9c8f9c9b6af8e
jembantonio <jembantonio@gmail.com>
2019-06-07 05:36:45 +0000

    initial commit

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+''' Student module
+'''
+
+
+class Student:
+    ''' initializes student class
+    '''
+    def __init__(self, first_name, last_name, age):
+        if type(first_name) is not str:
+            raise TypeError("first_name must be type str")
+        if type(last_name) is not str:
+            raise TypeError("last_name must be type str")
+        if type(age) is not int:
+            raise TypeError("last_name must be type int")
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        ''' retrieves dict of student instance
+        '''
+        if attrs is None:
+            return self.__dict__
+        if type(attrs) is not list:
+            return self.__dict__
+        return{i: self.__dict__[i] for i in attrs if i in self.__dict__.keys()}
