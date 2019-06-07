648d85b3f608edbf281abbf3409ddbd37fbf06f3
jembantonio <jembantonio@gmail.com>
2019-06-07 02:26:03 +0000

    adde typeerror edge case

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -4,9 +4,12 @@
 
 
 def read_lines(filename="", nb_lines=0):
+    if type(filename) is not str:
+        raise TypeError("file should be a string")
+
     with open(filename, 'r', encoding='utf-8') as file:
         if nb_lines <= 0:
             print(file.read(), end='')
-        while nb_lines:
+        while nb_lines != 0:
             print(file.readline(), end='')
             nb_lines -= 1
