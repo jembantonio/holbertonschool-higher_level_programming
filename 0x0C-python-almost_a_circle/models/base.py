108d0a8ae3a2e7554028b9d6c0ed7aab66862623
jembantonio <jembantonio@gmail.com>
2019-06-13 01:18:37 +0000

    added class method that saves to file the dictionary string json representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,16 @@ class Base:
         if list_dictionaries is None:
             return str([])
         return str(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        ''' writes the JSON string representation list to a file
+        '''
+        filename = (cls.__name__ + '.json')
+        if list_objs is None:
+            with open(filename, 'w', encoding='utf-8') as file:
+                file.write(Base.to_json_string([]))
+
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write(Base.to_json_string([i.to_dictionary()
+                                            for i in list_objs]))
