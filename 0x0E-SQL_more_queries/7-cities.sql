dc1022768b8b0574cde8c2cad3356524b3a811b6
jembantonio <jembantonio@gmail.com>
2019-07-16 19:51:02 +0000

    added condition

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- a script that creates the database hbtn_0d_usa and the table cities 
 -- (in the database hbtn_0d_usa) on your MySQL server
-CREATE DATABASE hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(id INT UNIQUE AUTO_INCREMENT NOT NULL,
 					      state_id INT NOT NULL,
