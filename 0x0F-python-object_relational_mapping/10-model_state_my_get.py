273c23e176a69e6518858a9ba8415b79a5421c99
jembantonio <jembantonio@gmail.com>
2019-08-12 05:54:11 +0000

    used try/except instead of if

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
                 sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    get_state = session.query(State).filter_by(name=sys.argv[4]).first()
-    if get_state:
+    try:
+        get_state = session.query(State).filter_by(name=sys.argv[4]).first()
         print(get_state.id)
-    else:
+    except:
         print("Not found")
