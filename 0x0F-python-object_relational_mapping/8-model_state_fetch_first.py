f94775d40c77102a76d4f174735da2ce28cb1e2a
jembantonio <jembantonio@gmail.com>
2019-08-12 05:48:49 +0000

    pep8 style

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,14 +8,13 @@ from model_state import Base, State
 import sys
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                            sys.argv[1], sys.argv[2], sys.argv[3]), 
-                            pool_pre_ping=True)
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(
+                sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
     first_state = session.query(State).order_by(State.id).first()
     if first_state:
-        print ("{}: {}".format(first_state.id, first_state.name))
+        print("{}: {}".format(first_state.id, first_state.name))
     else:
         print("Nothing")
-
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -8,10 +8,11 @@ from model_state import Base, State
 import sys
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                            sys.argv[1], sys.argv[2], sys.argv[3]),
-                            pool_pre_ping=True)
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(
+                sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).filter(State.name.like('%a%')).order_by(State.id):
+    for instance in session.query(State).filter(
+            State.name.like('%a%')).order_by(State.id):
         print("{}: {}".format(instance.id, instance.name))
