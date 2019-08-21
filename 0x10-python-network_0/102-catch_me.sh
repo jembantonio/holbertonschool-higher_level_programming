bb795351e60be7a5420c7f0d448ef7b53ccf7460
jembantonio <jembantonio@gmail.com>
2019-08-21 06:58:55 +0000

    removed comment line for line requiremnt

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # bash script that makses a request to 0.0.0.0:5000/catch_me
-# and causes the server to respond with a message "You got me!"
 curl -sLX PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
