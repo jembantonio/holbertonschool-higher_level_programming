14a8f70931fe9875bddf675ad474fd810a310bd8
jembantonio <jembantonio@gmail.com>
2019-08-21 07:01:50 +0000

    pep8 fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,17 +2,19 @@
 ''' Find the peak element
 '''
 
+
 def peak_binary(arr, low, hi):
-    #if array is empty
+    # if array is empty
     if low == hi:
         return arr[low]
 
-    #find index of middle element
+    # find index of middle element
     mid = low + (hi - low) // 2
-    #compare middle element with right elements
+    # compare middle element with right elements
     if arr[mid] > arr[mid + 1]:
         return peak_binary(arr, low, mid)
     return peak_binary(arr, mid + 1, hi)
+
 
 def find_peak(arr):
     if arr:
