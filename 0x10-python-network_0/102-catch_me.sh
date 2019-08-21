00ee45f3542c145a299b600137d45daad6f72558
jembantonio <jembantonio@gmail.com>
2019-08-21 06:55:43 +0000

    initial commit

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# bash script that makses a request to 0.0.0.0:5000/catch_me
+# and causes the server to respond with a message "You got me!"
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
