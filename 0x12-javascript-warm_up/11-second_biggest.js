177a72e7b04a0b6e00ebab10a0f8c4774b4f868c
jembantonio <jembantonio@gmail.com>
2019-09-18 05:50:13 +0000

    rid of var requirement

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 
-var arr = process.argv.slice(2);
-
-if (arr.length < 2) {
+if (process.argv.slice(2).length < 2) {
   console.log(0)
 } else {
-  arr.sort((a, b) => b - a);
-  console.log(arr[1]);
+  console.log(process.argv.slice(2).sort((a, b) => b - a)[1]);
 }
